b9af4aeb03bb4e47a2b1e54769c7f8b76cd961fb
Azmi Kamis <azmi.kamis@constantlycoding.com>
2021-01-06 11:13:37 +0800

    Update npmInstall.js

--- hooks/npmInstall.js
+++ hooks/npmInstall.js
@@ -1,17 +1,15 @@
-var child_process = require('child_process');
-
+console.log("Running hook to install hooks pre-requisites");
 module.exports = function (context) {
-    const Q = context.requireCordovaModule("q");
-	var deferral = Q.defer();
-
-	child_process.exec('npm install', {cwd:__dirname},
-		function (error) {
-			if (error !== null) {
-			  console.log('exec error: ' + error);
-			  deferral.reject('npm installation failed');
-			}
-			deferral.resolve();
-	});
-
-  return deferral.promise;
+  return new Promise((resolve, reject) => {
+    var child_process = require('child_process');
+    child_process.exec('npm install', {cwd: __dirname}, function (error) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        reject('npm installation failed');
+      }
+      else {
+        resolve();
+      }
+    });
+  });
 };
